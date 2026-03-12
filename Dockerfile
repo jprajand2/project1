996a6022159c5386348eb6f4ab8fb47beee73d1f
jprajand2-git <jprajand2@gmail.com>
2026-03-12 17:40:41 +0530

    fresh start

--- Dockerfile
+++ Dockerfile
@@ -2,4 +2,4 @@ FROM python:3.11
 WORKDIR /c/Users/george chellappa/project1
 COPY . .
 RUN pip install -r requirements.txt
-CMD ["python","program1.py"]
+CMD ["python","program2.py"]
--- program1.py
+++
@@ -1,5 +0,0 @@
-print ("1st display successful. Welcome!!!")
-print ("2nd line added via vs code")
-print ("newline appended")
-print ("this line added in github")
-#this is a comment added in git cmd
