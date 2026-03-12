17ba9c47b1addd7f7a48f00cd115ee7ded8a14c0
jprajand2-git <jprajand2@gmail.com>
2026-03-12 12:30:14 +0530

    one comment added in gitcmd

--- program1.py
+++ program1.py
@@ -1,3 +1,5 @@
 print ("1st display successful. Welcome!!!")
 print ("2nd line added via vs code")
 print ("newline appended")
+print ("this line added in github")
+#this is a comment added in git cmd
