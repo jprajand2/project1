3344980ebb2e1764a1e245f4a5a6fb80b5193108
jprajand2-git <jprajand2@gmail.com>
2026-03-15 13:09:53 +0530

    housekeeping

--- Dockerfile
+++ Dockerfile
@@ -2,4 +2,4 @@ FROM python:3.11
 WORKDIR ./project1
 COPY . /project1
 RUN pip install -r requirements.txt
-CMD ["python","program4.py"]
+CMD ["python","program5.py"]
---
+++ program5.py
@@ -0,0 +1,15 @@
+from http.server import SimpleHTTPRequestHandler, HTTPServer
+
+PORT = 8080
+
+class MyHandler(SimpleHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+        self.wfile.write(b"<h1>Hi from program5! NEW VERSION v2.0</h1>")
+
+server = HTTPServer(("0.0.0.0", PORT), MyHandler)
+
+print("Server running at http://localhost:8080")
+server.serve_forever()
